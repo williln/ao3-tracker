7a080f275059329bbd3622cfe2e1958570884832
Lacey Williams Henschel <laceynwilliams@gmail.com>
2022-10-20 09:40:03 -0700

    🏦 Changes to model fields

--- works/models.py
+++ works/models.py
@@ -5,8 +5,8 @@ from django_extensions.db.models import TimeStampedModel
 
 
 class Author(TimeStampedModel):
-    url = models.URLField()
-    username = models.CharField(max_length=100)
+    url = models.URLField(help_text="URL to the author")
+    username = models.CharField(max_length=100, help_text="Author's username")
 
     def __str__(self):
         return self.username
@@ -20,17 +20,24 @@ class Work(TimeStampedModel):
         DNF = "DNF", _("Did Not Finish")
 
     url = models.URLField()
+    platform_id = models.CharField(
+        null=True,
+        max_length=100,
+        help_text="ID of the work on the platform (AO3, FFN, etc.)",
+    )
     author = models.ForeignKey(
-        "works.Author", related_name="works", on_delete=models.SET_NULL, null=True
+        "works.Author",
+        related_name="works",
+        on_delete=models.SET_NULL,
+        null=True,
     )
     title = models.CharField(max_length=255)
     summary = models.TextField(null=True)
     complete = models.BooleanField(default=False)
     status = models.CharField(max_length=10, choices=Status.choices, default=Status.TBR)
     word_count = models.PositiveIntegerField(default=0)
-    posted = models.DateTimeField(null=True)
-    last_updated = models.DateTimeField(null=True)
-    completed = models.DateTimeField(null=True)
+    posted_date = models.DateTimeField(null=True)
+    last_updated_date = models.DateTimeField(null=True)
 
     def __str__(self):
         return f"{self.title} by {self.author.username}"
