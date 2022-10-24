a58f7026298bc708ae11d70e2c716570f82dbaad
Lacey Williams Henschel <laceynwilliams@gmail.com>
2022-10-24 11:13:34 -0700

    ✨ Add the admin and serve the static files

--- works/admin.py
+++ works/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from works.models import Author, Work
 
-# Register your models here.
+
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+
+class WorkAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Work, WorkAdmin)
