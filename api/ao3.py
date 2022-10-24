18a4f27c75fb526548ab75404df34eaa27c9dccd
Lacey Williams Henschel <laceynwilliams@gmail.com>
2022-10-24 12:29:40 -0700

    ✨ Get the "read" status of bookmarks

--- api/ao3.py
+++ api/ao3.py
@@ -2,17 +2,60 @@ from typing import List
 
 from AO3 import Session
 from AO3 import Work as AO3_Work
+from AO3.threadable import threadable
+from AO3.users import User as AO3_User
 from AO3.utils import workid_from_url
 from django.conf import settings
 
 from works.models import Author, Work
 
 
+class CustomSession(Session):
+    @threadable
+    def _load_bookmarks(self, page=1):
+        """
+        Override the default load_bookmarks method to add the ability to get
+        "recommended" status
+        """
+        url = self._bookmarks_url.format(self.username, page)
+        soup = self.request(url)
+        bookmarks = soup.find("ol", {"class": "bookmark index group"})
+        for bookm in bookmarks.find_all(
+            "li", {"class": ["bookmark", "index", "group"]}
+        ):
+            authors = []
+            recommended = False
+            workid = -1
+            if bookm.h4 is not None:
+                # Get the author and work ID
+                for a in bookm.h4.find_all("a"):
+                    if "rel" in a.attrs.keys():
+                        if "author" in a["rel"]:
+                            authors.append(AO3_User(str(a.string), load=False))
+                    elif a.attrs["href"].startswith("/works"):
+                        workname = str(a.string)
+                        workid = workid_from_url(a["href"])
+
+                # Get whether the bookmark is recommended
+                for span in bookm.p.find_all("span"):
+                    if "title" in span.attrs.keys():
+                        if span["title"] == "Rec":
+                            recommended = True
+
+                if workid != -1:
+                    new = AO3_Work(workid, load=False)
+                    setattr(new, "title", workname)
+                    setattr(new, "authors", authors)
+                    setattr(new, "recommended", recommended)
+                    if new not in self._bookmarks:
+                        self._bookmarks.append(new)
+
+
 def get_ao3_session() -> Session:
     """
     Returns an AO3 Session object
     """
-    return Session(settings.AO3_USERNAME, settings.AO3_PASSWORD)
+    return CustomSession(settings.AO3_USERNAME, settings.AO3_PASSWORD)
 
 
 def get_bookmarks(session: Session) -> list:
@@ -62,10 +105,15 @@ def import_bookmarks() -> List[AO3_Work]:
     counter = 0
 
     for bookmark in bookmarks:
-        if counter > 1:
-            break
         ao3_work = get_work(bookmark.url)
         ao3_author = ao3_work.authors[0]
+
+        # Note: If the work is "recommended," we make the assumption that the user
+        # has also read it and mark the status as READ.
+        if bookmark.recommended:
+            status = Work.Status.READ
+        else:
+            status = Work.Status.TBR
 
         # At this point, only gets first author
         author, _ = Author.objects.get_or_create(
@@ -78,11 +126,12 @@ def import_bookmarks() -> List[AO3_Work]:
                 "author": author,
                 "title": ao3_work.title,
                 "summary": ao3_work.summary,
+                "status": status,
                 "complete": ao3_work.complete,
                 "word_count": ao3_work.words,
                 "date_published": ao3_work.date_published,
                 "date_updated": ao3_work.date_edited,
-                "metdata": ao3_work.metadata
+                "metadata": ao3_work.metadata,
             },
         )
         counter += 1
